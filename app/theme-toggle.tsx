6d5c83bd4bd981bac146ca3156ca1dbe76b72a26
devJenny <devjenny.official@gmail.com>
2025-08-09 02:21:10 +0800

    Try.

--- app/theme-toggle.tsx
+++ app/theme-toggle.tsx
@@ -15,8 +15,8 @@ export function ThemeToggle() {
       className="rounded-full cursor-pointer relative"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <MdSunny className="absolute h-10 w-10 rotate-0 scale-120 dark:-rotate-90 dark:scale-0 transition-slow" />
-      <BsFillMoonStarsFill className="absolute h-10 w-10 rotate-90 scale-0 dark:-rotate-0 dark:scale-100 transition-slow" />
+      <MdSunny className="absolute h-10 w-10 rotate-0 scale-120 dark:-rotate-90 dark:scale-0 transition-default" />
+      <BsFillMoonStarsFill className="absolute h-10 w-10 rotate-90 scale-0 dark:-rotate-0 dark:scale-100 transition-default" />
     </Button>
   );
 }
