d5c8e29a80a6cfafb2856dd22d3519979efb792a
devJenny <devjenny.official@gmail.com>
2025-08-10 04:37:35 +0800

    Wip.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { ThemeToggle } from "./theme-toggle";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col justify-center items-center gap-5 bg-background transition-all duration-300">
-      <p className="text-4xl font-bold text-foreground">Hello world!</p>
-      <p className="text-xl text-secondary">How are you today?</p>
-      <ThemeToggle />
-    </div>
+    <main className="bg-background transition-default">
+      <Navbar />
+    </main>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,170 @@
+"use client";
+
+import { useState } from "react";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { FiMenu, FiX } from "react-icons/fi";
+import { MdSunny } from "react-icons/md";
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const navLinks = [
+  { label: "Home", href: "/homepage" },
+  { label: "Features", href: "#features" },
+  { label: "About", href: "#about" },
+  { label: "Contact", href: "#contact" },
+];
+
+const Navbar = () => {
+  const [active, setActive] = useState("Home");
+  const [isDark, setIsDark] = useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  return (
+    <nav className="sticky top-0 z-50 w-full md:h-20 h-16 bg-secondaryBackground border-b border-border flex justify-center">
+      <div
+        className="w-full max-w-[1280px] mx-auto h-full flex justify-between items-center sm:px-5 px-4"
+        role="navigation"
+      >
+        {/* Logo */}
+        <a href="/homepage" className="md:w-full md:max-w-43">
+          <Image
+            src="logo-light.svg"
+            alt="Logo Light"
+            className="block dark:hidden"
+            width={130}
+            height={40}
+            priority
+          />
+          <Image
+            src="logo-dark.svg"
+            alt="Logo Dark"
+            className="hidden dark:block"
+            width={130}
+            height={40}
+            priority
+          />
+        </a>
+
+        {/* Desktop Nav */}
+        <ul className="md:flex h-10 hidden">
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                href={link.href}
+                onClick={() => setActive(link.label)}
+                className={`px-4 h-full rounded-md text-sm border text-foreground flex justify-center items-center cursor-pointer
+                  ${
+                    active === link.label
+                      ? "bg-input border-border"
+                      : "border-transparent"
+                  }
+                `}
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        {/* Right Side */}
+        <div className="md:w-full md:max-w-43 flex md:gap-3 gap-2 h-10">
+          <button
+            onClick={() => {
+              setIsDark(!isDark);
+              document.documentElement.classList.toggle("dark");
+            }}
+            className="cursor-pointer w-10 h-10 flex justify-center items-center rounded-md text-foreground"
+          >
+            <AnimatePresence mode="wait" initial={false}>
+              {isDark ? (
+                <motion.div
+                  key="sun"
+                  initial={{ opacity: 0, rotate: -60 }}
+                  animate={{ opacity: 1, rotate: 0 }}
+                  exit={{ opacity: 0, rotate: 60 }}
+                  transition={{ duration: 0.15, ease: "easeInOut" }}
+                >
+                  <MdSunny size={25} />
+                </motion.div>
+              ) : (
+                <motion.div
+                  key="moon"
+                  initial={{ opacity: 0, rotate: -60 }}
+                  animate={{ opacity: 1, rotate: 0 }}
+                  exit={{ opacity: 0, rotate: 60 }}
+                  transition={{ duration: 0.15, ease: "easeInOut" }}
+                >
+                  <BsFillMoonStarsFill size={20} />
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </button>
+
+          <Button asChild className="md:block hidden">
+            <Link href="/login">Get Started</Link>
+          </Button>
+
+          <div className="md:hidden">
+            <button
+              onClick={() => setMobileOpen(!mobileOpen)}
+              className="h-9 px-2 bg-input border border-border rounded-md"
+            >
+              {mobileOpen ? (
+                <FiX className="text-xl text-foreground" />
+              ) : (
+                <FiMenu className="text-xl text-foreground" />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Nav */}
+      <AnimatePresence>
+        {mobileOpen && (
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{ type: "tween", duration: 0.3 }}
+            className="fixed inset-0 z-40 backdrop-blur-md bg-secondaryBackground md:hidden"
+          >
+            <button
+              onClick={() => setMobileOpen(false)}
+              className="absolute top-4 right-4 text-foreground text-2xl"
+            >
+              <FiX />
+            </button>
+
+            <div className="w-full h-full flex flex-col justify-center items-center gap-6">
+              {navLinks?.map((link, index) => (
+                <a
+                  key={index}
+                  href={link.href}
+                  onClick={() => {
+                    setActive(link.label);
+                    setMobileOpen(false);
+                  }}
+                  className={`text-foreground text-sm font-medium px-6 py-2 rounded-md border transition duration-150 ${
+                    active === link.label
+                      ? "bg-input border-border"
+                      : "border-transparent"
+                  }`}
+                >
+                  {link.label}
+                </a>
+              ))}
+              <Button asChild>
+                <Link href="/login">Get Started</Link>
+              </Button>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </nav>
+  );
+};
+
+export default Navbar;
