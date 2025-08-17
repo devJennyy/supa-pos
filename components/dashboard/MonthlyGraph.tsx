57bc913f4c7bdaeb966dce3ea1d1444a786228d9
devJenny <devjenny.official@gmail.com>
2025-08-18 07:25:44 +0800

    Remove extra div around Chart.

--- components/dashboard/MonthlyGraph.tsx
+++ components/dashboard/MonthlyGraph.tsx
@@ -6,9 +6,7 @@ const MonthlyGraph = () => {
   return (
     <div className="w-full flex-1 flex flex-col gap-4">
       <SectionTitle title="Monthly Profit" />
-      <div className="w-full rounded-xl">
-        <Chart />
-      </div>
+      <Chart />
     </div>
   );
 };
