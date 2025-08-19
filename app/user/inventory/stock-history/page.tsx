92d5d0a5084a4a6f4e2e52673a43c66b7e64e25c
devJenny <devjenny.official@gmail.com>
2025-08-19 21:26:18 +0800

    Update stock history ui.

--- app/user/inventory/stock-history/page.tsx
+++ app/user/inventory/stock-history/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 
-import React from "react";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import SectionTitle from "@/components/ui/section-title";
 import { Badge } from "@/components/ui/badge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card } from "@/components/ui/card";
+import { DatePicker } from "@/components/ui/date-picker";
+import { subDays } from "date-fns";
 
 export type StockHistoryItem = {
   id: string;
@@ -46,14 +56,21 @@ const history: StockHistoryItem[] = [
   },
 ];
 
-export default function StockHistoryPage({ data = history }: { data?: StockHistoryItem[] }) {
+export default function Page({
+  data = history,
+}: {
+  data?: StockHistoryItem[];
+}) {
   const [tab, setTab] = React.useState<"all" | "deducted" | "added">("all");
 
-  const filteredData = data.filter(item => {
+  const filteredData = data.filter((item) => {
     if (tab === "deducted") return item.action === "Deducted";
     if (tab === "added") return item.action === "Added";
     return true;
   });
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(
+    subDays(new Date(), 1)
+  );
 
   return (
     <main className="flex flex-1 flex-col gap-4 lg:p-5 p-4">
@@ -63,53 +80,135 @@ export default function StockHistoryPage({ data = history }: { data?: StockHisto
         direction="col"
       />
 
-      {/* Tabs */}
-      <Tabs value={tab} onValueChange={(value) => setTab(value as "all" | "deducted" | "added")}>
-        <TabsList>
-          <TabsTrigger value="all">All</TabsTrigger>
-          <TabsTrigger value="deducted">Deducted</TabsTrigger>
-          <TabsTrigger value="added">Added</TabsTrigger>
-        </TabsList>
-
-        <TabsContent value={tab}>
-          <Table className="!mt-3 rounded-sm border overflow-hidden">
-            <TableHeader className="sticky top-0 bg-input z-10 h-14">
-              <TableRow>
-                <TableHead className="w-[25%] px-5">Item</TableHead>
-                <TableHead className="w-[20%]">Category</TableHead>
-                <TableHead className="w-[20%]">Unit</TableHead>
-                <TableHead className="w-[15%]">Action</TableHead>
-                <TableHead className="w-[10%]">Quantity</TableHead>
-                <TableHead className="w-[20%]">Date & Time</TableHead>
-              </TableRow>
-            </TableHeader>
-
-            <TableBody className="bg-secondaryBackground">
-              {filteredData.map((item) => (
-                <TableRow key={item.id}>
-                  <TableCell className="font-medium px-5 text-secondary">{item.name}</TableCell>
-                  <TableCell>{item.category}</TableCell>
-                  <TableCell>{item.unitDescription || "unit"}</TableCell>
-                  <TableCell>
-                    <Badge variant={item.action === "Added" ? "default" : "destructive"}>
-                      {item.action}
-                    </Badge>
-                  </TableCell>
-                  <TableCell>{item.quantity}</TableCell>
-                  <TableCell className="pr-5">{item.timestamp}</TableCell>
-                </TableRow>
-              ))}
-
-              {filteredData.length === 0 && (
+      {/* Today's History */}
+      <Tabs
+        value={tab}
+        onValueChange={(value) => setTab(value as "all" | "deducted" | "added")}
+      >
+        <div className="flex lg:flex-row flex-col justify-between lg:!mb-1 !mb-2 gap-2">
+          <SectionTitle title="Today" />
+          <TabsList>
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="deducted">Deducted</TabsTrigger>
+            <TabsTrigger value="added">Added</TabsTrigger>
+          </TabsList>
+        </div>
+        <Card className="border border-border/50 px-5 bg-secondaryBackground/30">
+          <TabsContent value={tab}>
+            <Table className="rounded-xl border overflow-hidden">
+              <TableHeader className="sticky top-0 bg-input z-10 h-14">
                 <TableRow>
-                  <TableCell colSpan={6} className="text-center py-10 text-muted-foreground">
-                    No stock history available.
-                  </TableCell>
+                  <TableHead className="w-[25%] px-5">Item</TableHead>
+                  <TableHead className="w-[20%]">Category</TableHead>
+                  <TableHead className="w-[20%]">Unit</TableHead>
+                  <TableHead className="w-[15%]">Action</TableHead>
+                  <TableHead className="w-[10%]">Quantity</TableHead>
+                  <TableHead className="w-[20%]">Date & Time</TableHead>
                 </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TabsContent>
+              </TableHeader>
+
+              <TableBody className="bg-secondaryBackground">
+                {filteredData.map((item) => (
+                  <TableRow key={item.id}>
+                    <TableCell className="font-medium px-5 text-secondary h-14">
+                      {item.name}
+                    </TableCell>
+                    <TableCell>{item.category}</TableCell>
+                    <TableCell>{item.unitDescription || "unit"}</TableCell>
+                    <TableCell>
+                      <Badge
+                        variant={
+                          item.action === "Added" ? "default" : "destructive"
+                        }
+                      >
+                        {item.action}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>{item.quantity}</TableCell>
+                    <TableCell className="pr-5">{item.timestamp}</TableCell>
+                  </TableRow>
+                ))}
+
+                {filteredData.length === 0 && (
+                  <TableRow>
+                    <TableCell
+                      colSpan={6}
+                      className="text-center py-10 text-muted-foreground"
+                    >
+                      No stock history available.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TabsContent>
+        </Card>
+      </Tabs>
+
+      {/* Yesterday's History */}
+      <Tabs
+        value={tab}
+        onValueChange={(value) => setTab(value as "all" | "deducted" | "added")}
+        className="!mt-5"
+      >
+        <div className="flex lg:flex-row flex-col justify-between lg:!mb-1 !mb-2 gap-2">
+          <DatePicker date={selectedDate} setDate={setSelectedDate} />
+          <TabsList>
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="deducted">Deducted</TabsTrigger>
+            <TabsTrigger value="added">Added</TabsTrigger>
+          </TabsList>
+        </div>
+        <Card className="border border-border/50 px-5 bg-secondaryBackground/30">
+          <TabsContent value={tab}>
+            <Table className="rounded-xl border overflow-hidden">
+              <TableHeader className="sticky top-0 bg-input z-10 h-14">
+                <TableRow>
+                  <TableHead className="w-[25%] px-5">Item</TableHead>
+                  <TableHead className="w-[20%]">Category</TableHead>
+                  <TableHead className="w-[20%]">Unit</TableHead>
+                  <TableHead className="w-[15%]">Action</TableHead>
+                  <TableHead className="w-[10%]">Quantity</TableHead>
+                  <TableHead className="w-[20%]">Date & Time</TableHead>
+                </TableRow>
+              </TableHeader>
+
+              <TableBody className="bg-secondaryBackground">
+                {filteredData.map((item) => (
+                  <TableRow key={item.id}>
+                    <TableCell className="font-medium px-5 text-secondary h-14">
+                      {item.name}
+                    </TableCell>
+                    <TableCell>{item.category}</TableCell>
+                    <TableCell>{item.unitDescription || "unit"}</TableCell>
+                    <TableCell>
+                      <Badge
+                        variant={
+                          item.action === "Added" ? "default" : "destructive"
+                        }
+                      >
+                        {item.action}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>{item.quantity}</TableCell>
+                    <TableCell className="pr-5">{item.timestamp}</TableCell>
+                  </TableRow>
+                ))}
+
+                {filteredData.length === 0 && (
+                  <TableRow>
+                    <TableCell
+                      colSpan={6}
+                      className="text-center py-10 text-muted-foreground"
+                    >
+                      No stock history available.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TabsContent>
+        </Card>
       </Tabs>
     </main>
   );
