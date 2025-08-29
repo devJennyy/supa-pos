22bb9d4b83f094710b371e5697c0120d3dc58ca0
devJenny <devjenny.official@gmail.com>
2025-08-30 00:28:14 +0800

    Update :)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Navbar from "@/components/Navbar";
 
-export default function Home() {  
+export default function Home() {
   return (
     <main className="bg-background transition-default">
       <Navbar />
+      <p className="text-xl text-center !m-10">Currently work in progress!</p>
     </main>
   );
 }
