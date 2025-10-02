9e3244db1c31e456ad3bdbcbc2d178971d26d2f7
devJenny <devjenny.official@gmail.com>
2025-10-02 13:49:16 +0800

    Improve dashboard styling in light mode.

--- components/dashboard/OverviewCard.tsx
+++ components/dashboard/OverviewCard.tsx
@@ -10,6 +10,43 @@ import { FiTrendingUp } from "react-icons/fi";
 import SectionTitle from "@/components/ui/section-title";
 
 const OverviewCard = () => {
+  const stats = [
+    {
+      title: "Starting Change",
+      value: "5,000",
+      change: "4 Bills + 1000 coins",
+      changeLabel: "Prepared change",
+      icon: <FaCoins className="xl:text-[22px] text-sm text-primary" />,
+      type: "info",
+    },
+    {
+      title: "Sales In Cash",
+      value: "5,000",
+      change: "1.5%",
+      changeLabel: "Down from yesterday",
+      icon: <BsCashStack className="xl:text-[22px] text-sm text-primary" />,
+      type: "down",
+    },
+    {
+      title: "Sales in Banks",
+      value: "5,000",
+      change: "8.5%",
+      changeLabel: "Up from yesterday",
+      icon: (
+        <BsFillCreditCard2BackFill className="xl:text-[22px] text-sm text-primary" />
+      ),
+      type: "up",
+    },
+    {
+      title: "Profit",
+      value: "5,000",
+      change: "8.5%",
+      changeLabel: "Up from yesterday",
+      icon: <FiTrendingUp className="xl:text-2xl text-sm text-primary" />,
+      type: "up",
+    },
+  ];
+
   return (
     <div className="w-full flex flex-col gap-4">
       <SectionTitle
@@ -17,7 +54,7 @@ const OverviewCard = () => {
         label={format(new Date(), "EEEE, dd, MMMM yyyy")}
       />
 
-      <div className="grid auto-rows-min gap-4 lg:grid-cols-4 grid-cols-2">
+      {/* <div className="grid auto-rows-min gap-4 lg:grid-cols-4 grid-cols-2">
         <div className="relative bg-secondaryBackground border rounded-xl flex flex-col justify-between xl:p-5 p-3">
           <p className="xl:text-sm text-xs font-medium text-foreground">
             Starting Change
@@ -85,6 +122,47 @@ const OverviewCard = () => {
             <FiTrendingUp className="xl:text-2xl text-sm text-primary" />
           </div>
         </div>
+      </div> */}
+
+      <div className="grid auto-rows-min gap-4 lg:grid-cols-4 grid-cols-2">
+        {stats.map((item, index) => (
+          <div
+            key={index}
+            className="relative bg-input/30 dark:bg-secondaryBackground border border-borderBrand/50 dark:border-border rounded-xl flex flex-col justify-between xl:p-5 p-3"
+          >
+            <p className="xl:text-sm text-xs font-medium text-foreground">
+              {item.title}
+            </p>
+            <p className="xl:text-3xl text-2xl font-bold 2xl:my-12 xl:my-8 my-4">
+              {item.value}
+            </p>
+
+            <p className="xl:text-sm text-xs font-medium text-secondary flex xl:flex-row flex-col lg:gap-2 gap-1 whitespace-nowrap">
+              {item.type !== "info" && (
+                <span
+                  className={`flex gap-2 ${
+                    item.type === "up" ? "text-primary" : "text-destructive"
+                  }`}
+                >
+                  {item.type === "up" && <TrendingUp className="h-4 w-4" />}
+                  {item.type === "down" && <TrendingDown className="h-4 w-4" />}
+                  {item.change}
+                </span>
+              )}
+              {item.type === "info" && (
+                <span className="flex gap-2 text-secondary h-4 xl:hidden">
+                  <FaCoins className="xl:text-[22px] text-sm text-secondary sm:block hidden" />
+                  {item.change}
+                </span>
+              )}
+              {item.changeLabel}
+            </p>
+
+            <div className="absolute xl:top-5 xl:right-5 top-2.5 right-2.5 xl:w-12 xl:h-12 w-7 h-7 rounded-md bg-primary/5 dark:bg-input flex justify-center items-center">
+              {item.icon}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -26,7 +26,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-table-header dark:bg-secondaryBackground/50 text-muted-foreground inline-flex lg:h-10 h-9 border dark:border-border/20 w-fit items-center justify-center rounded-lg lg:p-1 p-[3.5px]",
+        "bg-input/40 dark:bg-secondaryBackground/50 text-muted-foreground inline-flex lg:h-10 h-9 border border-borderBrand/80 dark:border-border/20 w-fit items-center justify-center rounded-lg lg:p-1 p-[3.5px]",
         className
       )}
       {...props}
@@ -42,7 +42,7 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-border/60 dark:data-[state=active]:bg-input/50 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-sm border border-transparent px-4 lg:py-3.5 py-3 lg:text-sm text-[13px] font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "data-[state=active]:bg-primary dark:data-[state=active]:text-foreground data-[state=active]:text-white focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-border/60 dark:data-[state=active]:bg-input/50 text-secondary dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-sm border border-transparent px-4 lg:py-3.5 py-3 lg:text-sm text-[13px] font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
       {...props}
